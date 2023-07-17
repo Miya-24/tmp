5ba0a30a2e6507113884f0f3dbccf495094cc1bb
Miyazawa <37150304+Miya-24@users.noreply.github.com>
2023-07-18 01:28:05 +0900

    Create Main.cpp

---
+++ CheckTest/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "CheckIF.h"
+
+int main()
+{
+	std::cout << "--- CheckTest1 ---" << std::endl;
+	CheckTest1();
+	std::cout << std::endl;
+
+	std::cout << "--- CheckTest2 ---" << std::endl;
+	CheckTest2();
+	std::cout << std::endl;
+
+	return 0;
+}
